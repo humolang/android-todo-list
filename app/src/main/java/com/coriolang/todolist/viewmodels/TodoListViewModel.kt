178df33ef7b9d9255b237251b4573daad26ba93f
coriolang <yan.radi@gmail.com>
2022-08-06 15:05:23 +0300

    Thread-safe methods for StateFlow<TodoItem>

--- app/src/main/java/com/coriolang/todolist/viewmodels/TodoListViewModel.kt
+++ app/src/main/java/com/coriolang/todolist/viewmodels/TodoListViewModel.kt
@@ -5,23 +5,23 @@ import androidx.lifecycle.viewModelScope
 import com.coriolang.todolist.data.todoItem.Importance
 import com.coriolang.todolist.data.todoItem.TodoItem
 import com.coriolang.todolist.data.todoItem.TodoItemDao
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 class TodoListViewModel(private val todoItemDao: TodoItemDao) : ViewModel() {
 
     private val _todoItem = MutableStateFlow(TodoItem())
-    val todoItem: StateFlow<TodoItem> = _todoItem
+    val todoItem = _todoItem.asStateFlow()
 
     fun insertTodoItem() {
         val currentTime = System.currentTimeMillis()
 
-        _todoItem.value = _todoItem.value.copy(
-            creationDate = currentTime,
-            modificationDate = currentTime
-        )
+        _todoItem.update {
+            it.copy(
+                creationDate = currentTime,
+                modificationDate = currentTime
+            )
+        }
 
         viewModelScope.launch {
             todoItemDao.insert(todoItem.value)
@@ -29,9 +29,9 @@ class TodoListViewModel(private val todoItemDao: TodoItemDao) : ViewModel() {
     }
 
     fun updateTodoItem() {
-        _todoItem.value = _todoItem.value.copy(
-            modificationDate = System.currentTimeMillis()
-        )
+        _todoItem.update {
+            it.copy(modificationDate = System.currentTimeMillis())
+        }
 
         viewModelScope.launch {
             todoItemDao.update(todoItem.value)
@@ -46,31 +46,34 @@ class TodoListViewModel(private val todoItemDao: TodoItemDao) : ViewModel() {
 
     fun findTodoItemById(id: Int) {
         viewModelScope.launch {
-            _todoItem.value = todoItemDao.findById(id)
+            _todoItem.update {
+                todoItemDao.findById(id)
+            }
         }
     }
 
     fun allTodoItems(): Flow<List<TodoItem>> = todoItemDao.findAll()
 
     fun defaultTodoItem() {
-        _todoItem.value = TodoItem()
+        _todoItem.update { TodoItem() }
     }
 
     fun setTodoText(text: String) {
-        _todoItem.value = _todoItem.value.copy(
-            text = text
-        )
+        _todoItem.update {
+            it.copy(text = text)
+        }
     }
 
     fun setTodoImportance(importance: Importance) {
-        _todoItem.value = _todoItem.value.copy(
-            importance = importance
-        )
+        _todoItem.update {
+            it.copy(importance = importance)
+        }
     }
 
     fun setTodoIsCompleted(id: Int, isCompleted: Boolean) {
         viewModelScope.launch {
             var todoItem = todoItemDao.findById(id)
+
             todoItem = todoItem.copy(
                 isCompleted = isCompleted,
                 modificationDate = System.currentTimeMillis()
@@ -81,8 +84,8 @@ class TodoListViewModel(private val todoItemDao: TodoItemDao) : ViewModel() {
     }
 
     fun setTodoDeadlineDate(deadlineDate: Long) {
-        _todoItem.value = _todoItem.value.copy(
-            deadlineDate = deadlineDate
-        )
+        _todoItem.update {
+            it.copy(deadlineDate = deadlineDate)
+        }
     }
 }
